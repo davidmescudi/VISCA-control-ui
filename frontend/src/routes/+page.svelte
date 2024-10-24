a4222725d4aa46ba66b5c874f26f72a3c65ce5d9
dnabbe <social@davidabbe.de>
2024-10-25 00:37:39 +0200

    add simple tailwind component index site

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>
+  <div class="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+    <h2 class="text-center text-base/7 font-semibold text-orange-600">VISCA Protocol</h2>
+    <p class="mx-auto mt-2 max-w-lg text-balance text-center text-4xl font-semibold tracking-tight text-neutral-300 sm:text-5xl">Camera Control</p>
+  </div>
+</div>
